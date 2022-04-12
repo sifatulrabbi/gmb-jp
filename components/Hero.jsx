069257d8a69bbea7432cbaf2a071e1260204f47a
sifatulrabbi <mdsifatulislam.rabbi@gmail.com>
2022-04-12 13:20:06 +0600

    feat: add hero section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,11 +1,51 @@
 // dependencies
 import React from "react";
+// components
+import Image from "next/image";
+import { heroImg, topLogo } from "../static";
 
 // main component
 function Hero() {
   return (
-    <div>
-      <h1>集客の景色を 変えます</h1>
+    <div className="w-full h-screen flex flex-col">
+      {/* hero top */}
+      <div className="flex flex-row items-start gap-12">
+        {/* hero left logo image */}
+        <div className="max-w-[20%]">
+          <Image src={topLogo} alt="" layout="intrinsic" />
+        </div>
+
+        {/* hero right */}
+        <div className="flex flex-col gap-8 mt-[20vh] pr-4">
+          {/* hero right top text */}
+          <h1 className="font-bold text-3xl">
+            集客の景色を
+            <br />
+            変えます
+          </h1>
+
+          {/* hero right image */}
+          <div className="overflow-hidden">
+            <Image
+              src={heroImg}
+              alt=""
+              layout="intrinsic"
+              className="object-cover"
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* hero bottom */}
+      <div className="w-full p-4">
+        <ul className="flex flex-col pl-4 pb-12 border-l-[1px] border-black">
+          <li>
+            <span className="font-english">GMBJAPAN</span>は
+          </li>
+          <li>あなたのお店に</li>
+          <li>お客様を呼ぶ仕掛けを作ります</li>
+        </ul>
+      </div>
     </div>
   );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,13 @@
+// dependencies
+import React from "react";
+// components
+import Hero from "../components/Hero";
+
+// main component
 export default function Home() {
-  return <div className="h-full w-full"></div>;
+  return (
+    <div className="h-full w-full">
+      <Hero />
+    </div>
+  );
 }
