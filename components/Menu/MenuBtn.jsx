df5972d4726c908000ea336c178bad7e5384ad58
sifatulrabbi <mdsifatulislam.rabbi@gmail.com>
2022-04-15 22:27:48 +0600

    add animation to the menu button

--- components/Menu/MenuBtn.jsx
+++ components/Menu/MenuBtn.jsx
@@ -6,21 +6,29 @@ import { v4 } from "uuid";
 function MenuBtn({ show, toggleMenu }) {
   return (
     <button
-      className={`z-[1000] h-full flex w-[64px] gap-[3px] right-6 justify-center`}
+      className={`relative z-[1000] h-[7vw] min-h-[60px] flex w-[64px] gap-[3px] right-6 justify-center menu-btn ${
+        show ? "show" : ""
+      }`}
       onClick={toggleMenu}
     >
-      {[1, 2, 3, 4, 5].map(() => (
+      {[0, 1, 2, 3, 4].map((item) => (
         <div
           key={v4()}
-          className={`h-full relative w-[1px] bg-black 
+          className={`menu-btn-bar h-full relative w-[1px] bg-black 
           before:absolute before:top-0 before:right-0 before:left-0 before:bottom-0 before:bg-black 
           ${
             show
               ? "rotate-[35deg] before:-rotate-[70deg] bg-[#a0a0a0] before:bg-[#a0a0a0]"
               : "rotate-0 before:rotate-0"
           }`}
+          style={{ animationDelay: `${item / 20}s` }}
         ></div>
       ))}
+      <div
+        className={`menu-btn-text uppercase tracking-widest text-sm font-biz writing-vertical hidden md:block absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0`}
+      >
+        Menu
+      </div>
     </button>
   );
 }
